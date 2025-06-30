20120062a2d5bdece7cf231710891c34285cdc15
DamianoRufa <122522304+DamianoRufa@users.noreply.github.com>
2025-06-30 19:08:24 +0200

    Sistema Quest Personaggi
    i personaggi escono ed entrano dopo le loro richieste
    Bisogna sistemare le animazioni di uscita e i tempi

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Animator characterAnimator;        // Animator del personaggio
     public Animator bubbleAnimator;           // Animator della nuvoletta
     public Animator fadeOut_Animator;
 
@@ -18,7 +17,12 @@ public class GameManager : MonoBehaviour
     private bool hasStarted = false;
 
     public GameObject slot;                     // Lo slot dove avviene il drop
-    public string requiredTag = "HolyStrenght"; // Il tag richiesto per il drop
+
+    // Tag richiesti per ogni personaggio
+    public string requiredTagCharacter1 = "HolyStrenght";
+    public string requiredTagCharacter2 = "MagicPower";
+    public string requiredTagCharacter3 = "SpeedBoost";
+
     public string finalMessage;
 
     public Animator characterExitAnimator;      // Animator del personaggio che va via
@@ -35,7 +39,19 @@ public class GameManager : MonoBehaviour
     {
         if (!hasStarted)
         {
-            StartCoroutine(PlaySequence());
+            if (currentCharacter != null)
+            {
+                Animator currentAnimator = currentCharacter.GetComponent<Animator>();
+                if (currentAnimator != null)
+                {
+                    if (currentCharacter.CompareTag("Character1"))
+                        StartCoroutine(PlaySequenceCharacter1(currentAnimator));
+                    else if (currentCharacter.CompareTag("Character2"))
+                        StartCoroutine(PlaySequenceCharacter2(currentAnimator));
+                    else if (currentCharacter.CompareTag("Character3"))
+                        StartCoroutine(PlaySequenceCharacter3(currentAnimator));
+                }
+            }
             hasStarted = true;
         }
     }
@@ -64,45 +80,76 @@ public class GameManager : MonoBehaviour
         sfondoNero.gameObject.SetActive(false);
     }
 
-    IEnumerator PlaySequence()
+    IEnumerator PlaySequenceCharacter1(Animator characterAnim)
     {
         yield return new WaitForSeconds(1f);
-        characterAnimator.Play("Mario_Animation");
+        characterAnim.Play("Mario_Animation");
         yield return new WaitForSeconds(2f);
-        yield return StartCoroutine(PlayBubbleAnimation());
+
+        bubbleAnimator.gameObject.SetActive(true);
+        characterAnim.Play("Mario_StandBy");
+        yield return new WaitForSeconds(1f);
+
         typingEffect.StartTyping(messageToDisplay);
         yield return new WaitForSeconds(1f);
-
     }
 
-    IEnumerator PlayBubbleAnimation()
+    IEnumerator PlaySequenceCharacter2(Animator characterAnim)
     {
+        yield return new WaitForSeconds(1f);
+        characterAnim.Play("Student_Animation");
+        yield return new WaitForSeconds(2f);
+
         bubbleAnimator.gameObject.SetActive(true);
-        characterAnimator.Play("Mario_StandBy");
+        characterAnim.Play("Student_StandBy");
+        yield return new WaitForSeconds(1f);
+
+        typingEffect.StartTyping(messageToDisplay);
         yield return new WaitForSeconds(1f);
     }
 
+    IEnumerator PlaySequenceCharacter3(Animator characterAnim)
+    {
+        yield return new WaitForSeconds(1f);
+        characterAnim.Play("Lady_Animation");
+        yield return new WaitForSeconds(2f);
+
+        bubbleAnimator.gameObject.SetActive(true);
+        characterAnim.Play("Lady_StandBy");
+        yield return new WaitForSeconds(1f);
+
+        typingEffect.StartTyping(messageToDisplay);
+        yield return new WaitForSeconds(1f);
+    }
 
     public void OnButtonClicked()
     {
-            if (slot.transform.childCount > 0)
-            {
-                GameObject droppedObject = slot.transform.GetChild(0).gameObject;
+        if (slot.transform.childCount > 0)
+        {
+            GameObject droppedObject = slot.transform.GetChild(0).gameObject;
 
-                if (droppedObject.CompareTag(requiredTag))
-                {
-                    StartCoroutine(HandleCorrectDropSequence());
-                }
-                else
-                {
-                    Debug.Log("Oggetto nel slot ha un tag sbagliato.");
-                }
+            string requiredTag = "";
+
+            if (currentCharacter.CompareTag("Character1"))
+                requiredTag = requiredTagCharacter1;
+            else if (currentCharacter.CompareTag("Character2"))
+                requiredTag = requiredTagCharacter2;
+            else if (currentCharacter.CompareTag("Character3"))
+                requiredTag = requiredTagCharacter3;
+
+            if (droppedObject.CompareTag(requiredTag))
+            {
+                StartCoroutine(HandleCorrectDropSequence());
             }
             else
             {
-                Debug.Log("Nessun oggetto presente nello slot.");
+                Debug.Log("Oggetto nel slot ha un tag sbagliato.");
             }
-        
+        }
+        else
+        {
+            Debug.Log("Nessun oggetto presente nello slot.");
+        }
     }
 
     IEnumerator HandleCorrectDropSequence()
@@ -121,7 +168,19 @@ public class GameManager : MonoBehaviour
             currentCharacter.SetActive(false);
 
         if (nextCharacter != null)
+        {
             nextCharacter.SetActive(true);
+            Animator nextCharAnimator = nextCharacter.GetComponent<Animator>();
+            if (nextCharAnimator != null)
+            {
+                if (nextCharacter.CompareTag("Character1"))
+                    yield return StartCoroutine(PlaySequenceCharacter1(nextCharAnimator));
+                else if (nextCharacter.CompareTag("Character2"))
+                    yield return StartCoroutine(PlaySequenceCharacter2(nextCharAnimator));
+                else if (nextCharacter.CompareTag("Character3"))
+                    yield return StartCoroutine(PlaySequenceCharacter3(nextCharAnimator));
+            }
+        }
     }
 }
 
